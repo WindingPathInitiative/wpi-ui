c3f6513282b55f13ff4956b784b834cfbd71e2b9
Joe Terranova <joeterranova@gmail.com>
2026-07-14 02:48:53 +0000

    Update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   keycloak: {
-    authority: 'https://cognito-prod.us-east-1.amazonaws.com/us-east-1_Z8w7iYiTa',
-    clientId: '1f29bqp2uftofs1boitt279s15',
+    authority: 'https://auth.windingpath.club/realms/wpi',
+    clientId: 'wpi-ui',
   },
   hub: {
-    url: 'https://hub.api.windingpath.club/v1/',
+    url: 'https://hub.windingpath.club/v1/',
   },
 };
